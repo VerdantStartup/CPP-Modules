9eea6dcd550989905a51be96d5b2de6735ebb286
Marius Santiago Wilsch <84270258+VerdantStartup@users.noreply.github.com>
2023-04-30 08:49:06 +0200

    Finished ex04

---
+++ cpp01/ex04/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: verdant <verdant@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/29 19:31:41 by verdant           #+#    #+#             */
+/*   Updated: 2023/04/30 08:48:01 by verdant          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "replace.hpp"
+
+// 1. Open file
+// 2. Read file
+// 3. Replace occurence of s1 by s2
+// 4. Write to new file
+// 5. Close file
+
+
+int main(void)
+{
+	Replace r1("test", "test", "TEST");
+}
